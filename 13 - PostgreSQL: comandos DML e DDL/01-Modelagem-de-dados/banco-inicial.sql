401e8c1201b7678df4fcf115153e9bc1f6153797
Vinicius999 <viniciussilva.dsc@gmail.com>
2023-02-25 15:26:41 -0300

    Starting 13 - PostgreSQL

---
+++ 13 - PostgreSQL: comandos DML e DDL/01-Modelagem-de-dados/banco-inicial.sql
@@ -0,0 +1,52 @@
+CREATE DATABASE alura;
+
+CREATE SCHEMA academico;
+DROP TABLE aluno, curso, aluno_curso, categoria;
+
+CREATE TABLE academico.aluno (
+    id SERIAL PRIMARY KEY,
+	primeiro_nome VARCHAR(255) NOT NULL,
+	ultimo_nome VARCHAR(255) NOT NULL,
+	data_nascimento DATE NOT NULL
+);
+
+CREATE TABLE academico.categoria (
+    id SERIAL PRIMARY KEY,
+	nome VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE academico.curso (
+    id SERIAL PRIMARY KEY,
+	nome VARCHAR(255) NOT NULL,
+	categoria_id INTEGER NOT NULL REFERENCES academico.categoria(id)
+);
+
+CREATE TABLE academico.aluno_curso (
+	aluno_id INTEGER NOT NULL REFERENCES academico.aluno(id),
+	curso_id INTEGER NOT NULL REFERENCES academico.curso(id),
+	PRIMARY KEY (aluno_id, curso_id)
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+CREATE TABLE aluno_curso (
+	aluno_id INTEGER NOT NULL REFERENCES aluno(id),
+	curso_id INTEGER NOT NULL REFERENCES curso(id),
+	PRIMARY KEY (aluno_id, curso_id)
+);
+
---
+++ 13 - PostgreSQL: comandos DML e DDL/README.md
@@ -0,0 +1,37 @@
+# PostgreSQL: Views, Sub-Consultas e Funções
+
+## Aula 01 - Modelagem de Dados
+
+#### Objetivos:
+
+- Conhecemos o conceito de modelagem de dados;
+- Conhecemos sobre schemas no PostgreSQL que nos ajudam a organizar nossas tabelas;
+- Conversamos sobre análise de requisitos e sobre modelos utilizados neste processo;
+
+## Aula 02 - Estrutura do banco
+
+#### Objetivos:
+
+- a
+- b
+- c
+
+## Aula 03 - C
+
+#### Objetivos:
+
+- a
+- b
+- c
+
+## Aula 04 - D
+
+#### Objetivos:
+
+- a
+- b
+
+## Aula 05 - E
+
+- a
+- b
