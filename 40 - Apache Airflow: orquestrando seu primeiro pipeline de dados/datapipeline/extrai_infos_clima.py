b5fef29bf4305232324edbb3c4b137ff0fbd2bb8
Vinicius999 <viniciussilva.dsc@gmail.com>
2023-05-06 19:33:45 -0300

    40 - Apache Airflow: start pipeline project

---
+++ 40 - Apache Airflow: orquestrando seu primeiro pipeline de dados/datapipeline/extrai_infos_clima.py
@@ -0,0 +1,21 @@
+import os
+from os.path import join
+import pandas as pd
+from datetime import datetime, timedelta
+
+# intervalo de datas
+data_inicio = datetime.today()
+data_fim = data_inicio + timedelta(days=7)
+
+# formatando as datas
+data_inicio = data_inicio.strftime('%Y-%m-%d')
+data_fim = data_fim.strftime('%Y-%m-%d')
+
+city = 'Boston'
+key = 'YFB3BN8XU7LVJ63XF77E97RPC'
+
+URL = join('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/',
+            f'{city}/{data_inicio}/{data_fim}?unitGroup=metric&include=days&key={key}&contentType=csv')
+
+dados = pd.read_csv(URL)
+print(dados.head())
